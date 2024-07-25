a459be20deb2b04968b254a5a142487a583f80a5
Ethen1264 <jxorhks86909763@gmail.com>
2024-07-25 13:03:56 +0900

    💄Update style

--- src/stories/atoms/Contact/index.tsx
+++ src/stories/atoms/Contact/index.tsx
@@ -2,10 +2,19 @@ import Github from '../../icons/Github'
 import * as S from './style'
 
 const Contact = ({ ...props }) => {
+  const handleLinkClick = (url: string) => {
+    window.open(url, '_blank')
+  }
+
   return (
     <S.Wrapper {...props}>
       <S.Discord>Discord: Yozora1264</S.Discord>
-      <S.Github to='https://github.com/Ethen1264/TodayWhat-new-client'>
+      <S.Github
+        as='a'
+        onClick={() =>
+          handleLinkClick('https://github.com/Ethen1264/TodayWhat-new-client')
+        }
+      >
         <Github />
       </S.Github>
     </S.Wrapper>
--- src/stories/atoms/ScheduleList/index.tsx
+++ src/stories/atoms/ScheduleList/index.tsx
@@ -1,17 +1,18 @@
-import { Stroke } from '../../assets/svg'
+import { ReactNode } from 'react'
 import * as S from './style'
 
 interface Props {
   time?: string
+  icons?: ReactNode
   subject?: string
 }
 
-const ScheduleList: React.FC<Props> = ({ time, subject, ...props }) => {
+const ScheduleList: React.FC<Props> = ({ time, icons, subject, ...props }) => {
   return (
     <S.Wrapper {...props}>
       <S.TextContainer>
         <S.TimeText>{time}</S.TimeText>
-        <Stroke />
+        {icons}
         <S.SubjectText>{subject}</S.SubjectText>
       </S.TextContainer>
     </S.Wrapper>
