7908248719e05c33612050fe2d0affdee1f8d189
Ethen1264 <jxorhks86909763@gmail.com>
2025-03-04 19:44:42 +0900

    add: 쿠키 만료 문제 해결

--- src/util/expirationCookie.ts
+++ src/util/expirationCookie.ts
@@ -1,5 +1,12 @@
 export const expirationCookie = (): Date => {
   const now: Date = new Date()
-  const expirationDate: Date = new Date(now.getFullYear(), 2, 1, 23, 59, 59)
+  let expirationDate: Date
+
+  if (now.getMonth() < 2 || (now.getMonth() === 2 && now.getDate() === 1)) {
+    expirationDate = new Date(now.getFullYear(), 2, 1)
+  } else {
+    expirationDate = new Date(now.getFullYear() + 1, 2, 1)
+  }
+
   return expirationDate
 }
